459bd2f656fc682aba881f46d994dda96d6523aa
Nils <52067471+nils-n@users.noreply.github.com>
2024-02-15 19:35:10 +0000

    update font family to Montserrat

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import smallReactLogo from "./react-logo.png";
-
 import "@fontsource-variable/montserrat";
 import "./App.css";
 
